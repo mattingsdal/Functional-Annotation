c89c2a679562b7c1b4167e79d20c354209ef1f43
Morten Mattingsdal <morten.mattingsdal@gmail.com>
2016-05-24 13:08:06 +0200

    Update InterProScan.sh

--- InterProScan.sh
+++ InterProScan.sh
@@ -1,22 +1,3 @@
-#!/bin/bash
+# running interproscan on abel.uio.no
 
-# Job name:
-#SBATCH --job-name=Testinterpro
-#
-# Project:
-#SBATCH --account=cees
-#
-# Wall clock limit:
-# Max memory usage:
-#SBATCH --mem-per-cpu=4G
-#SBATCH --time=00:12:00
-## Set up job environment
-source /cluster/bin/jobsetup
-module load interproscan
-
-## Copy input files to the work directory:
-cp /usit/abel/u1/mortema/genome/test_genome.maker.output/test_genome.all.maker.proteins.fasta $SCRATCH
-
-## Do some work:
-cd $SCRATCH
-interproscan.sh -dp -i test_genome.all.maker.proteins.fasta -f tsv -b ~/resultr3 -T /tmp/
+srun --mem-per-cpu=2G --time=04:00:00 --account=cees ~/software/interproscan-5.18-57.0/interproscan.sh -dp -i test_genome.all.maker.proteins.fasta -f tsv -b /usit/abel/u1/mortema/genome/test_genome.maker.output/interproscan -T /usit/abel/u1/mortema/tmp
