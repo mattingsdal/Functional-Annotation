83f346fa9176c73eae4926a90fa873f586c83217
Morten Mattingsdal <morten.mattingsdal@gmail.com>
2016-05-24 14:24:33 +0200

    Update InterProScan.sh

--- InterProScan.sh
+++ InterProScan.sh
@@ -1,8 +1,10 @@
 # running interproscan on abel.uio.no
 
+module load python2
+
 srun --mem-per-cpu=4G --time=04:00:00 --account=cees \
 ~/software/interproscan-5.18-57.0/interproscan.sh \
 -dp -i test_genome.all.maker.proteins.fasta -f tsv \
 -b /usit/abel/u1/mortema/genome/test_genome.maker.output/interproscan \
---goterms  -appl Pfam -mode cluster -clusterrunid u666\
+--goterms -mode cluster -clusterrunid interproscan\
 -T /usit/abel/u1/mortema/tmp
