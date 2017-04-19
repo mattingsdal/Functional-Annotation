35a07265e34849ddb8a814270c0ee839f30a20fa
Morten Mattingsdal <morten.mattingsdal@gmail.com>
2017-04-19 10:52:58 +0200

    Update 01.InterProScan.sh

--- 01.InterProScan.sh
+++ 01.InterProScan.sh
@@ -1,12 +1,16 @@
-# running interproscan in cluster mode on abel.uio.no
-# requires that interproscan.properties are edited to srun
+# running interproscan in standalone mode on 
+# requires that interproscan.properties are edited
 
 module load python2
 
-srun --mem-per-cpu=4G --time=04:00:00 --account=cees --cpus-per-task=8 --ntasks=1 \
-~/software/interproscan-5.18-57.0/interproscan.sh \
--dp -i test_genome.all.maker.proteins.fasta -f tsv,xml,gff3,html \
--b /usit/abel/u1/mortema/genome/test_genome.maker.output/interproscan \
---goterms -mode cluster -clusterrunid interproscan \
+~/software/interproscan-5.18-57.0/interproscan.sh -dp -i /usit/abel/u1/mortema/MAKER/Symphodus_melops.maker.output/Symphodus_melops.all.maker.proteins.aed.1.0.fasta \
+-f tsv,xml,gff3,html -appl Pfam -b /usit/abel/u1/mortema/MAKER/interproscan -iprlookup --goterms -mode standalone \
 -T /usit/abel/u1/mortema/tmp
 
+# 
+iprscan2gff3
+
+#
+ipr_update_gff
+
+#
