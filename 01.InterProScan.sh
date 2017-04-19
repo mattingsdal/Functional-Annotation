c17876d18b7d34c0caaed8243e3a03e307f961c0
Morten Mattingsdal <morten.mattingsdal@gmail.com>
2017-04-19 12:32:32 +0200

    Update 01.InterProScan.sh

--- 01.InterProScan.sh
+++ 01.InterProScan.sh
@@ -11,6 +11,10 @@ module load python2
 iprscan2gff3
 
 #
-ipr_update_gff
+ipr_update_gff ../Symphodus_melops.maker.output/Symphodus_melops.all.gff.aed-1.0.gff interproscan.tsv >Symphodus_melops.all.gff.aed-1.0.wPFAM.gff
 
-#
+perl -lne 'print $1 if /\tmRNA\t.+ID=([^;]+).+Pfam(.+?);/ ' Symphodus_melops.all.gff.aed-1.0.wPFAM.gff >Symphodus_melops.all.gff.aed-1.0.only.PFAM.ids
+
+# extract gene predictions with Pfam domains
+../fgrep.pl -f Symphodus_melops.all.gff.aed-1.0.only.PFAM.ids Symphodus_melops.all.gff.aed-1.0.wPFAM.gff -d "=|;|,|:" >Symphodus_melops.all.gff.aed-1.0.onlyPFAM.gff
+
